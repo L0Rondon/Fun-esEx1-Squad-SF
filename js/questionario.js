c2979a48d46de348154f0337699ee904b1e2a05f
SENAI Zerbini <aluno@escolar509.sp.senai.br>
2025-02-11 12:47:37 -0300

    V2

--- js/questionario.js
+++ js/questionario.js
@@ -1,9 +1,56 @@
 const idade = parseInt(prompt("Qual a sua idade: "))
-const Cidade = parseInt(prompt("Quantos a sua cidade: "))
+const Cidade = (prompt("Qual a sua cidade: "))
 const mesNasc = parseInt(prompt("Qual o mês em que nasceu (1,2,3...): "))
-if(idade >= 18){
-    if()
-}
-else{
+if (idade >= 18) {
+    if (Cidade == "Campinas") {
+        switch (mesNasc) {
+            case 1:
+                mes = false;
+                break;
+            case 2:
+                mes = true;
+                break;
+            case 3:
+                mes = false;
+                break;
+            case 4:
+                mes = true;
+                break;
+            case 5:
+                mes = false;
+                break;
+            case 6:
+                mes = true;
+                break;
+            case 7:
+                mes = false;
+                break;
+            case 8:
+                mes = true;
+                break;
+            case 9:
+                mes = true;
+                break;
+            case 10:
+                mes = false;
+                break;
+            case 11:
+                mes = false;
+                break;
+            case 12:
+                mes = false;
+                break;
+        }
+        if (mes == true) {
+            document.getElementById('permissãoEntrada').textContent = "Entrada Permitida";
+        } else {
+            document.getElementById('permissãoEntrada').textContent = "A entrada Não foi permitida";
+        }
 
+    } else {
+        document.getElementById('permissãoEntrada').textContent = "A entrada Não foi permitida";
+    }
+}
+else {
+    document.getElementById('permissãoEntrada').textContent = "A entrada Não foi permitida";
 }
